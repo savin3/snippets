515d699bc4ddb3dda70c7c4ee04081a9c846593d
savin ilya <savin15ilya3112@gmail.com>
2025-11-20 21:30:05 +0700

    fix: views classes

--- snippets/views.py
+++ snippets/views.py
@@ -3,15 +3,24 @@ from django.contrib.auth.models import User
 from .models import Snippet
 from .serializers import SnippetSerializer, UserSerializer
 
+from rest_framework import permissions
+
 
 class SnippetList(generics.ListCreateAPIView):
    queryset = Snippet.objects.all()
    serializer_class = SnippetSerializer
+   permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+   def perform_create(self, serializer):
+       serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
    queryset = Snippet.objects.all()
    serializer_class = SnippetSerializer
+   permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
 
 class UserList(generics.ListAPIView):
    queryset = User.objects.all()
